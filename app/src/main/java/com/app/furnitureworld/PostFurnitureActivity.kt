21f96f4669c5b256642c90f78811b12d70ae6cd9
Shanmuk-palnati <56579843+Shanmuk-palnati@users.noreply.github.com>
2023-10-27 23:25:59 -0500

    Code logic for OrderHistory, PostFurniture and Profile - Shanmuk Palnati - 10/27

---
+++ app/src/main/java/com/app/furnitureworld/PostFurnitureActivity.kt
@@ -0,0 +1,11 @@
+package com.app.furnitureworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostFurnitureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_furniture)
+    }
+}
