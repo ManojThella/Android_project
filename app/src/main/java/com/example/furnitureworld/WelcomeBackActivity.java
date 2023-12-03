9821ee02673ac1bda8dfcb9a4c9f2c9bb942ef96
S559249 <123011251+S559249@users.noreply.github.com>
2023-12-02 23:23:51 -0600

    Updated welcome back page

--- app/src/main/java/com/example/furnitureworld/WelcomeBackActivity.java
+++ app/src/main/java/com/example/furnitureworld/WelcomeBackActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class WelcomeBackActivity extends AppCompatActivity {
 
@@ -16,21 +20,17 @@ public class WelcomeBackActivity extends AppCompatActivity {
         Button postButton = findViewById(R.id.postButton);
         Button browseButton = findViewById(R.id.browseButton);
 
-        // Set onClick functionality for the "Post Furniture Details" button
         postButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Navigate to PostFurnitureActivity
                 Intent intent = new Intent(WelcomeBackActivity.this, PostFurnitureActivity.class);
                 startActivity(intent);
             }
         });
 
-        // Set onClick functionality for the "Browse Posted Items" button
         browseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Navigate to BrowseItemsActivity
                 Intent intent = new Intent(WelcomeBackActivity.this, BrowseFurnitureActivity.class);
                 startActivity(intent);
             }
